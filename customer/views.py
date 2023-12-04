c9142260181c506a43f76a6fa6716ab03c0c1716
iammeechar <aldomicha@gmail.com>
2023-12-04 21:02:33 +0300

    Added various item categories for item variation

--- customer/views.py
+++ customer/views.py
@@ -11,3 +11,9 @@ class About(View):
      def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
 
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        pass
+        #get each item from each category
+        #pass int context
+        #render the template
